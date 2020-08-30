4b2c52a078465dcf103898ef444dcda945fd91dc
Seba1705 <sebaaguirre2012@gmail.com>
2020-08-30 13:32:11 -0300

    Se prueba loading

--- PruebaApp/src/app/pages/login/login.page.ts
+++ PruebaApp/src/app/pages/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/shared/user.class';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
     selector: 'app-login',
@@ -11,16 +12,28 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginPage implements OnInit {
 
     user = new User();
+    loading: any;
 
-    constructor(private router: Router, private auth: AuthService) { }
+    constructor(private router: Router, private auth: AuthService, private loadingCtrl: LoadingController) { }
 
     ngOnInit() {
     }
 
     async login() {
+        this.presentLoading('Cargando...');
         const user = await this.auth.onLogin(this.user);
 
-        if (user)
+        if (user) {
             this.router.navigateByUrl('/user');
+            this.loading.dismiss();
+        }
+    }
+
+    async presentLoading(message: string) {
+        this.loading = await this.loadingCtrl.create({
+            message,
+            spinner: "circles"
+        });
+        return this.loading.present();
     }
 }
